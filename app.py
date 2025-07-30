7cf8b14d39cfbc473c76e06a6b2b708f1d7acd0d
AlesiaUku <alesiauku@icloud.com>
2025-07-30 12:30:52 +0200

    Sistemova modelin dhe vectorizer-in e ri

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import joblib
 
-# Ngarko modelin Passive Aggressive dhe vectorizer-in
+# Ngarko modelin dhe vectorizerin
 model = joblib.load("pac_model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
-# Konfigurimi i faqes
+# UI
 st.set_page_config(page_title="Detektues Lajmesh", layout="centered")
 st.title("🎓 Universiteti Politeknik i Tiranës")
 st.header("📘 Detektues i Lajmeve të Rreme")
@@ -14,10 +14,8 @@ st.markdown("""
 Ky aplikacion përdor algoritmin **Passive Aggressive Classifier** për të zbuluar nëse një lajm është i **vërtetë** apo i **rremë**, në bazë të përmbajtjes së tij.
 """)
 
-# Fusha për të shtypur lajmin
 news = st.text_area("📰 Shkruani lajmin që dëshironi të verifikoni:")
 
-# Butoni për verifikim
 if st.button("🔍 Verifiko lajmin"):
     if news.strip() == "":
         st.warning("Ju lutem shkruani një lajm për ta analizuar.")
@@ -26,10 +24,9 @@ if st.button("🔍 Verifiko lajmin"):
         prediction = model.predict(x)
 
         if prediction[0] == "FAKE":
-            st.error("🎉 Ky është një LAJM I RREMË!")
+            st.error("🚨 Ky është një LAJM I RREMË!")
         else:
             st.success("✅ Ky është një LAJM I VËRTETË!")
 
-# Footer
 st.markdown("---")
 st.caption("Punuar si pjesë e temës së diplomës në Fakultetin e Teknologjisë së Informacionit.")
